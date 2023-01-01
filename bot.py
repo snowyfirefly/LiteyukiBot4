b7e22eaf3dedf284d065150d52353d3cbd1f0d46
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2023-01-02 01:02:27 +0800

    Merge remote-tracking branch 'origin/master'

--- bot.py
+++ bot.py
@@ -16,11 +16,11 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 nonebot.load_from_toml("pyproject.toml")
+for plugin_name in Data(Data.globals, "liteyuki").get_data("installed_plugin", []):
+    try:
+        nonebot.load_plugin(plugin_name)
+    except BaseException as e:
+        nonebot.logger.info("插件：%s导入时出现错误:%s" % plugin_name % traceback.format_exception(e))
 
 if __name__ == "__main__":
-    for plugin_name in Data(Data.globals, "liteyuki").get_data("installed_plugin", []):
-        try:
-            nonebot.load_plugin(plugin_name)
-        except BaseException as e:
-            nonebot.logger.info("插件：%s导入时出现错误:%s" % plugin_name % traceback.format_exception(e))
     nonebot.run(app="__mp_main__:app")

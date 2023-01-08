05e0455e6901bca92f0c27901059537303e7471d
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2023-01-08 13:16:09 +0800

    天气插件更新了一半

--- src/liteyuki_plugins/liteyuki_base/system.py
+++ src/liteyuki_plugins/liteyuki_base/system.py
@@ -23,8 +23,8 @@ from nonebot.adapters.onebot.v11 import GroupMessageEvent, PrivateMessageEvent, 
 from nonebot.permission import SUPERUSER
 import pickle
 
-check_update = on_command("检查更新", permission=SUPERUSER)
-set_auto_update = on_command("启用自动更新", aliases={"停用自动更新"}, permission=SUPERUSER)
+check_update = on_command("#检查更新", permission=SUPERUSER)
+set_auto_update = on_command("#启用自动更新", aliases={"#停用自动更新"}, permission=SUPERUSER)
 update = on_command("#update", aliases={"#更新", "#轻雪更新"}, permission=SUPERUSER)
 restart = on_command("#reboot", aliases={"#重启", "#轻雪重启"}, permission=SUPERUSER)
 export_database = on_command("#export", aliases={"#导出数据"}, permission=SUPERUSER)
@@ -45,7 +45,7 @@ async def _(bot: Bot, event: Union[GroupMessageEvent, PrivateMessageEvent]):
     resp_data = resp.json()
     msg = "当前版本：%s(%s)\n仓库版本：%s(%s)" % (local_version_name, local_version_id, resp_data.get("version_name"), resp_data.get("version_id"))
     if resp_data.get("version_id") > local_version_id:
-        msg += "\n检测到新版本：\n请使用「#update BotQQ号」命令手动更新"
+        msg += "\n检测到新版本：\n请使用「#更新」命令手动更新"
     await check_update.send(msg)
 
 

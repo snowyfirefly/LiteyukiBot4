321d4850edbb09bc284eccea3e9b0a9f398dd52f
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2023-01-09 01:20:15 +0800

    天气插件更新了一半

--- src/liteyuki_plugins/liteyuki_plugin_weather/resource.py
+++ src/liteyuki_plugins/liteyuki_plugin_weather/resource.py
@@ -1,4 +1,5 @@
 resource_git = {
     "src/resource/textures/qweather/sunrise.png": "snowyfirefly/Liteyuki-Resource/raw/master/textures/qweather/sunrise.png",
     "src/resource/textures/qweather/sunset.png": "snowyfirefly/Liteyuki-Resource/raw/master/textures/qweather/sunset.png",
+    "src/resource/textures/qweather/white_dot.png": "snowyfirefly/Liteyuki-Resource/raw/master/textures/qweather//white_dot.png",
 }
